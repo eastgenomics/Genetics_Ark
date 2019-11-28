52dd4716fd2d7d1c39dd4207dcbb55a3ce3a275c
Yu-jinKim <yujin.kim@hotmail.fr>
2019-11-28 16:25:50 +0000

    renaming, formatting

--- primer_db/models.py
+++ primer_db/models.py
@@ -11,7 +11,7 @@ from django.core.exceptions import ValidationError
 # set all to null=True for testing forms
 
 class PrimerDetails(models.Model):
-    primer_name = models.CharField(unique=True, max_length=50)
+    name = models.CharField(unique=True, max_length=50)
     gene = models.CharField(max_length=15)
     sequence = models.CharField(max_length=100)
     gc_percent = models.FloatField(verbose_name='GC %')
@@ -23,20 +23,14 @@ class PrimerDetails(models.Model):
     snp_info = models.TextField(max_length=500, null=True, blank = False)
     snp_date = models.DateField(null = True, blank = True)
     status = models.ForeignKey("Status")
-    scientist = models.ForeignKey("Scientist" )
-    pcr_program = models.ForeignKey("PCRProgram", verbose_name='PCR Program', )
-    buffer = models.ForeignKey("Buffer" )
+    scientist = models.ForeignKey("Scientist")
+    pcr_program = models.ForeignKey("PCRProgram", verbose_name='PCR Program')
+    buffer = models.ForeignKey("Buffer")
     coordinates = models.ForeignKey("Coordinates", on_delete=models.CASCADE)
     pairs = models.ForeignKey("Pairs", blank=True, null=True)
 
     def __str__(self):
-        """String for representing the Model object."""
-        return ('{} {} {} {} {} {} {} {} {} {} {} {}'.format(
-        self.primer_name, self.sequence, self.gc_percent, self.tm,
-        self.comments, self.arrival_date, self.location, self.status, 
-        self.scientist, self.pcr_program, self.buffer, self.coordinates)
-
-                )
+        return self.name
 
 
 class Coordinates(models.Model):
@@ -47,9 +41,7 @@ class Coordinates(models.Model):
     start_coordinate_38 = models.IntegerField(verbose_name = 'GRCh38 Start', null=True)
     end_coordinate_38 = models.IntegerField(verbose_name = 'GRCh38 End',  null=True)
     
-
     def __str__(self):
-        """String for representing the Model object."""
         return '{} {} {} {} {}'.format(self.chrom_no, self.start_coordinate_37, 
             self.end_coordinate_37, self.start_coordinate_38, self.end_coordinate_38)
 
@@ -57,46 +49,33 @@ class Coordinates(models.Model):
 class Pairs(models.Model):
     coverage_37 = models.CharField(max_length = 50, verbose_name = 'GRCh37 Coverage', null = True)
     coverage_38 = models.CharField(max_length = 50, verbose_name = 'GRCh38 Coverage', null = True)
-    #length = models.IntegerField()
-
-        
 
 
 class Status(models.Model):
-    status = models.CharField(max_length=200)   
+    name = models.CharField(max_length=200)   
 
     def __str__(self):
-            """String for representing the Model object."""
-            return str(self.status)
+        return self.name
 
     
-
 class Scientist(models.Model):
     forename = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
 
-
     def __str__(self):
-            """String for representing the Model object."""
-            return '{} {}'.format(self.forename, self.surname)
-
+        return '{} {}'.format(self.forename, self.surname)
 
 
 class PCRProgram(models.Model):
-    pcr_program = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
-        """String for representing the Model object."""
-        return str(self.pcr_program)
-
+        return self.name
 
 
 class Buffer(models.Model):
-    buffer = models.CharField(max_length=200)
-
+    name = models.CharField(max_length=200)
 
     def __str__(self):
-        """String for representing the Model object."""
-        return str(self.buffer)
-
+        return self.name
 
