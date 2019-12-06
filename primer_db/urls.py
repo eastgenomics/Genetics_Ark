a2d3bdaca869f5c3cc42f271817b495610d6568f
jethror1 <rainford1995@gmail.com>
2019-12-06 16:35:40 +0000

    start of help page

--- primer_db/urls.py
+++ primer_db/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, include
 from django.views.generic import RedirectView
 from . import views
-
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
                # Default view if the user has not navigated yet
                url(r'^$', views.index, name='primer_db_index'),
 
+               url(r'^help/$', TemplateView.as_view(template_name='primer_db/help.html'), name='help_page'),           
 
                url(r'^submit/$', views.submit, name = 'submit'),
 
