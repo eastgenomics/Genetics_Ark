c7b7d6a0f284cf42a264afff9d51857cf570c5aa
Yu-jinKim <yujin.kim@hotmail.fr>
2019-12-11 15:41:54 +0000

    added subpop allele frequencies + "logging" for every batch checking

--- utils/snp_check.py
+++ utils/snp_check.py
@@ -6,8 +6,6 @@ To check every primer:
 cd genetics_ark_django/utils
 ml django
 python snp_check.py
-
-
 """
 
 import datetime
@@ -26,6 +24,33 @@ django.setup()
 
 import primer_db.models as Models
 
+ALLELE_FREQUENCY_THRESHOLD = 0.005
+
+
+def get_snp(ref, snp, primer_start, primer_end):
+    ref2gnomad = {"37": "r2_1", "38": "r3"}
+
+    true_snps = []
+
+    if primer_start <= snp['pos'] <= primer_end:
+        for region_type in ["exome", "genome"]:
+            if not snp[region_type]:
+                continue
+
+            for pop in snp[region_type]["populations"]:
+                if pop["an"] == 0:
+                    continue
+
+                pop_af = pop["ac"] / pop["an"]
+
+                if pop_af <= ALLELE_FREQUENCY_THRESHOLD and pop_af != 0:
+                    snp_pos = snp['pos'] - primer_start
+                    gnomad_link = "{}?dataset=gnomad_{}".format(snp['variant_id'], ref2gnomad[ref])
+                    true_snps.append("+{}, {}".format(snp_pos, gnomad_link))
+
+    return true_snps
+                
+
 def main(gene, primer_start_37, primer_end_37, primer_start_38, primer_end_38):
     snp_date = datetime.datetime.now().strftime("%Y-%m-%d")
     snp_info = []
@@ -35,16 +60,8 @@ def main(gene, primer_start_37, primer_end_37, primer_start_38, primer_end_38):
 
         if total_snps:
             for snp in total_snps:
-                if ref == "37":
-                    if primer_start_37 <= snp['pos'] <= primer_end_37:
-                        snp_pos = snp['pos'] - primer_start_37
-                        gnomad_link = "{}?dataset=gnomad_r2_1".format(snp['variant_id'])
-                        snp_info.append("+{}, {}".format(snp_pos, gnomad_link))
-                elif ref == "38":
-                    if primer_start_38 <= snp['pos'] <= primer_end_38:
-                        snp_pos = snp['pos'] - primer_start_38
-                        gnomad_link = "{}?dataset=gnomad_r3".format(snp['variant_id'])
-                        snp_info.append("+{}, {}".format(snp_pos, gnomad_link))
+                snp_info += get_snp(ref, snp, primer_start_37, primer_end_37)
+                snp_info += get_snp(ref, snp, primer_start_38, primer_end_38)
 
     if snp_info:
         snp_status = 2
@@ -54,43 +71,55 @@ def main(gene, primer_start_37, primer_end_37, primer_start_38, primer_end_38):
     return snp_status, snp_date, snp_info
 
 if __name__ == "__main__":
+    today = datetime.datetime.now().strftime("%y%m%d")
+    record_file_path = "snp_checking_update/{}".format(today)
+
+    if not os.path.exists("snp_checking_update"):
+        os.mkdir("snp_checking_update")
+
     primers = Models.PrimerDetails.objects.all()
 
-    for primer in primers:
-        current_snp_status = primer.snp_status
-        current_snp_info = primer.snp_info
-        
-        if current_snp_info:
-            current_snp_info = current_snp_info.split(";")
-        else:
-            current_snp_info = []
+    with open(record_file_path, "a") as f:
+        for primer in primers:
+            f.write("{}:\n".format(primer))
+            current_snp_status = primer.snp_status
+            current_snp_info = primer.snp_info
 
-        new_status, new_snp_date, new_snp_info = main(
-            primer.gene,
-            primer.coordinates.start_coordinate_37,
-            primer.coordinates.end_coordinate_37,
-            primer.coordinates.start_coordinate_38,
-            primer.coordinates.end_coordinate_38
-        )
+            f.write(" - Current snps: {}\n".format(current_snp_info))
 
-        new_snps = current_snp_info
-
-        for new_snp in new_snp_info:
-            if current_snp_status == 0:
-                new_snps.append(new_snp)
+            if current_snp_info:
+                current_snp_info = current_snp_info.split(";")
             else:
-                if new_snp not in current_snp_info:
+                current_snp_info = []
+
+            new_status, new_snp_date, new_snp_info = main(
+                primer.gene,
+                primer.coordinates.start_coordinate_37,
+                primer.coordinates.end_coordinate_37,
+                primer.coordinates.start_coordinate_38,
+                primer.coordinates.end_coordinate_38
+            )
+
+            new_snps = current_snp_info
+
+            for new_snp in new_snp_info:
+                if current_snp_status == 0:
                     new_snps.append(new_snp)
+                else:
+                    if new_snp not in current_snp_info:
+                        new_snps.append(new_snp)
 
-        if new_snps:
-            new_snps = ";".join(set(new_snps))
-            primer.snp_status = "2"
-        else:
-            new_snps = ""
-            primer.snp_status = "1"
+            if new_snps:
+                new_snps = ";".join(set(new_snps))
+                f.write(" - New snps detected: {}\n".format(new_snp_info))
+                primer.snp_status = "2"
+            else:
+                f.write(" - No new snps\n")
+                new_snps = ""
+                primer.snp_status = "1"
 
-        primer.snp_info = new_snps
-        primer.snp_date = datetime.datetime.now()
-        primer.save()
+            primer.snp_info = new_snps
+            primer.snp_date = datetime.datetime.now()
+            primer.save()
 
-        time.sleep(0.0001)
+            time.sleep(0.0001)
