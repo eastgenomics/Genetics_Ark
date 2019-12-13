905fe37a5565bca6a6f3ee5f7a4d8912836f7c84
Yu-jinKim <yujin.kim@hotmail.fr>
2019-12-13 15:08:23 +0000

    updated to handle old symbol/aliases using HGNC

--- utils/snp_check.py
+++ utils/snp_check.py
@@ -24,7 +24,31 @@ django.setup()
 
 import primer_db.models as Models
 
+sys.path.append("/mnt/storage/home/kimy/projects/HGNC_api/bin/")
+
+import HGNC_api
+
 ALLELE_FREQUENCY_THRESHOLD = 0.005
+
+
+def get_hgnc_symbol(gene_name):
+    # check if given gene name is in gene list, if not use API to find new symbol from HGNC
+    new_gene = HGNC_api.get_new_symbol(gene_name)
+
+    if not new_gene:
+        # if no new symbol retrieved, check aliases
+        alias_list = HGNC_api.get_alias(gene_name)
+
+        if alias_list:
+            for alias in alias_list:
+                for ref in ["37", "38"]:
+                    query_res = gnomAD_queries.snp_check_query(alias, ref)
+    
+                    if query_res:
+                        alias_id = HGNC_api.get_id(alias, verbose=False)
+                        new_gene = HGNC_api.get_symbol_from_id(alias_id, verbose=False)
+
+    return new_gene
 
 
 def get_snp(ref, snp, primer_start, primer_end):
@@ -58,13 +82,19 @@ def main(gene, primer_start_37, primer_end_37, primer_start_38, primer_end_38):
     for ref in ["37", "38"]:
         total_snps = gnomAD_queries.snp_check_query(gene, ref)
 
-        if total_snps:
-            for snp in total_snps:
-                snp_info += get_snp(ref, snp, primer_start_37, primer_end_37)
-                snp_info += get_snp(ref, snp, primer_start_38, primer_end_38)
+        if not total_snps:
+            new_gene = get_hgnc_symbol(gene)
+            
+            if new_gene:
+                total_snps = gnomAD_queries.snp_check_query(new_gene, ref)
 
-        else:
-            snp_info += ["Gene not recognized"]
+                if not total_snps:
+                    snp_info += ["Gene not recognized"]
+                    continue
+
+        for snp in total_snps:
+            snp_info += get_snp(ref, snp, primer_start_37, primer_end_37)
+            snp_info += get_snp(ref, snp, primer_start_38, primer_end_38)
 
     if snp_info:
         if "Gene not recognized" in snp_info:
@@ -86,8 +116,8 @@ if __name__ == "__main__":
     primers = Models.PrimerDetails.objects.all()
 
     with open(record_file_path, "a") as f:
-        for primer in primers:
-            print(primer)
+        for i, primer in enumerate(primers):
+            print(i, primer)
             f.write("{}:\n".format(primer))
             current_snp_status = primer.snp_status
             current_snp_info = primer.snp_info
@@ -118,7 +148,7 @@ if __name__ == "__main__":
 
             if new_snps:
                 if "Gene not recognized" in new_snps:
-                    f.write(" - Gene not recognized")
+                    f.write(" - Gene not recognized\n")
                 else:
                     f.write(" - New snps detected: {}\n".format(new_snp_info))
                 new_snps = ";".join(set(new_snps))
