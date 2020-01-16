82b6df38f7c6cd5f7ada24eda1bac3476d008b9f
Yu-jinKim <yujin.kim@hotmail.fr>
2020-01-16 10:24:09 +0000

    completely refactored script

--- utils/smalt_mapping.py
+++ utils/smalt_mapping.py
@@ -1,8 +1,9 @@
+from collections import defaultdict
 import django
 import os
+import re
 import sys
 import subprocess
-
 
 import gnomAD_queries
 
@@ -13,68 +14,150 @@ django.setup()
 
 import primer_db.models as Models
 
+SMALT_FASTA = "primers_smalt.fasta"
+REF_37 = "/mnt/storage/data/refs/homo_sapiens/GRCh37/Homo_sapiens_assembly37"
+REF_38 = "/mnt/storage/data/refs/homo_sapiens/GRCh38/Homo_sapiens_assembly38"
 
-def main():
 
-    with open(primers_file, 'w+') as primer_fasta:
+def multiple_mapping_check(*primers, temp_fasta = SMALT_FASTA):
+    """ Given primers model objects, align and add comments """
+
+    with open(temp_fasta, "w") as f:
         for primer in primers:
-            primer_fasta.write(">{}\n{}\n".format(primer.name, primer.sequence))
-        
-        primer_fasta.close()
+            f.write(">{}\n{}\n".format(primer.name, primer.sequence))
 
-    chrom = primer.coordinates.chrom_no
+    success = main(temp_fasta)
+
+    if success:
+        print("SUCCESS")
+        sys.exit(0)
+    else:
+        print("FAILED")
+        sys.exit(1)
+
+
+def run_smalt(cmd):
+    print("MAPPING...")
+    print(cmd)
+    process = subprocess.run(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+
+    if process.returncode != 0:
+        print("SMALT issue: \"{}\"".format(process.stderr.decode("ascii").strip()))
+        return False
     
-    ref_37 = "/mnt/storage/data/refs/homo_sapiens/GRCh37/Homo_sapiens_assembly37"
-    ref_38 = "/mnt/storage/data/refs/homo_sapiens/GRCh38/Homo_sapiens_assembly38"
+    smalt_output = process.stdout.decode("ascii").strip()
+    smalt_output = [line for line in smalt_output.split("\n") if not line.startswith("@")]
 
-    cmd_37 = "smalt map -d -1 -m 15 {} {}".format(ref_37, primers_file)
-    cmd_38 = "smalt map -d -1 -m 15 {} {}".format(ref_38, primers_file)
+    # for line in smalt_output:
+    #     print(line.split("\t"))
 
-    smalt_out_37 = subprocess.run(cmd_37, shell = True, stdout = subprocess.PIPE).stdout.decode("ascii").strip()
-    smalt_out_37 = [line for line in smalt_out_37.split("\n") if not line.startswith("@")]
-    #print(smalt_out_37)
-    
-    count = 0
+    print("PARSING SMALT OUTPUT...")
 
-    for line in smalt_out_37:
+    kept_primers = {}
+
+    for line in smalt_output:
         line = line.split('\t')
-        print(line)
+        name = line[0]
+        primer = Models.PrimerDetails.objects.get(name = name)
+        smalt_chrom = line[2]
+        smalt_seq = line[9]
+        smalt_nm_tag = line[11]
+        smalt_as_tag = line[12]
+        smalt_cigar = line[5]
 
-        match = int(len(line[9]) - int(line[12].split(':')[2]))
+        if not smalt_as_tag.startswith("AS"):
+            print("AS tag not at expected position")
+            return False
 
-        if match <= 5:
-            print(match)
+        if not smalt_nm_tag.startswith("NM"):
+            print("NM tag not at expected position")
+            return False
 
-            count +=1
-    
-    
-    print(count)
-                
+        # get just primers on correct chromosome
+        if primer.coordinates.chrom_no == smalt_chrom:
+            length_minus_alignscore = len(smalt_seq) - int(smalt_as_tag.split(':')[2])
+
+            if re.search(r"[^M0-9]", smalt_cigar):
+                parsing_cigar = []
+                cigar_op = ""
+
+                for char in smalt_cigar:
+                    if not char.isdigit():
+                        parsing_cigar.append((int(cigar_op), char))
+                        cigar_op = ""
+                    else:
+                        cigar_op += char
+
+                if parsing_cigar[-1][1] not in ["M", "="]:
+                    continue
+                else:
+                    if parsing_cigar[-1][0] <= 15:
+                        continue
+
+            if length_minus_alignscore <= 5:
+                kept_primers.setdefault(primer, []).append(line)
+
+    return kept_primers
 
 
+def check_nb_mapping(primer2mappings):
+    comment = "Multiple mapping detected, check before use"
 
-    # smalt_out_38 = subprocess.run(cmd_38, shell = True, stdout = subprocess.PIPE).stdout.decode("ascii").strip()
-    # smalt_out_38 = [line for line in smalt_out_38.split("\n") if not line.startswith("@")]
+    for primer, mappings in primer2mappings.items():
+        if len(mappings) >= 3:
+            if primer.pairs_id:
+                paired_primers = Models.PrimerDetails.objects.filter(pairs_id = primer.pairs_id)
 
-    for line in smalt_out_38:
-        line = line.split('\t')
+                if len(paired_primers) != 2:
+                    print("{} have this pair id {}".format(len(paired_primers), primer.pairs_id))
+                    return False
+                else:
+                    primers_to_comment = paired_primers
 
-        match = 0
+            else:
+                primers_to_comment = primer
 
-        if chrom == line[2]:
-            # get just primers on correct chromosome
-            match = int(len(line[9]) - int(line[12].split(':')[2]))
-        
-            if match <= 5:
-                match_list2.append(match)
+            for primer_to_comment in primers_to_comment:
+                primer_to_update = Models.PrimerDetails.objects.filter(name = primer_to_comment.name)
+
+                if primer_to_update[0].comments:
+                    if comment not in primer_to_update[0].comments:
+                        new_comment = "{}\n{}".format(primer_to_update[0].comments, comment)
+                    else:
+                        continue
+                else:
+                    new_comment = comment
+
+                # primer_to_update.update(**{"comments": new_comment})
+
+    return True
 
 
+def main(temp_fasta):
+    all_mappings = []
+
+    cmd_37 = "smalt map -d -1 -m 15 {} {}".format(REF_37, temp_fasta)
+    cmd_38 = "smalt map -d -1 -m 15 {} {}".format(REF_38, temp_fasta)
+
+    for cmd in [cmd_37, cmd_38]:
+        all_mappings.append(run_smalt(cmd))
+
+    # if all(all_mappings):
+    #     print("CHECKING NB MAPPINGS PER PRIMER...")
+
+    #     for mapping_dict in all_mappings:
+    #         success = check_nb_mapping(mapping_dict)
+
+    #         if not success:
+    #             return False
+    # else:
+    #     print("SMALT issue")
+    #     return False
+
+    # return True
 
 
 
 if __name__ == "__main__":
     primers = Models.PrimerDetails.objects.all()
-
-    primers_file = "smalt_temp.fasta"
-
-    main()
+    multiple_mapping_check(*primers)
