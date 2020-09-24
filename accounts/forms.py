1a2de05579852a1516076bbd55fce3a7119f98ab
jethror1 <rainford1995@gmail.com>
2020-09-24 16:59:04 +0100

    formatting

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,23 +8,42 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 username_validator = UnicodeUsernameValidator()
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=12, min_length=4, required=True, help_text='Required: First Name',
-                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=12, min_length=4, required=True, help_text='Required: Last Name',
-                               widget=(forms.TextInput(attrs={'class': 'form-control'})))
-    email = forms.EmailField(max_length=50, help_text='Required. Inform a valid email address.',
-                             widget=(forms.TextInput(attrs={'class': 'form-control'})))
-    password1 = forms.CharField(label=_('Password'),
-                                widget=(forms.PasswordInput(attrs={'class': 'form-control'})),
-                                help_text=password_validation.password_validators_help_text_html())
-    password2 = forms.CharField(label=_('Password Confirmation'), widget=forms.PasswordInput(attrs={'class': 'form-control'}),
-                                help_text=_('Just Enter the same password, for confirmation'))
+    first_name = forms.CharField(
+        max_length=12, min_length=4, required=True, 
+        help_text='Required: First Name',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control', 'placeholder': 'First Name'
+
+        })
+    )
+    last_name = forms.CharField(
+        max_length=12, min_length=4, required=True,
+        help_text='Required: Last Name',
+        widget=(forms.TextInput(attrs={'class': 'form-control'}))
+    )
+    email = forms.EmailField(
+        max_length=50, help_text='Required. Inform a valid email address.',
+        widget=(forms.TextInput(attrs={'class': 'form-control'}))
+    )
+    password1 = forms.CharField(
+        label=_('Password'),
+        widget=(forms.PasswordInput(attrs={'class': 'form-control'})),
+        help_text=password_validation.password_validators_help_text_html()
+    )
+    password2 = forms.CharField(
+        label=_('Password Confirmation'), 
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        help_text=_('Just Enter the same password, for confirmation')
+    )
     username = forms.CharField(
         label=_('Username'),
         max_length=150,
-        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        help_text=_('Required. 150 characters or fewer. Letters, digits and\
+            @/./+/-/_ only.'),
         validators=[username_validator],
-        error_messages={'unique': _("A user with that username already exists.")},
+        error_messages={
+            'unique': _("A user with that username already exists.")
+        },
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
