cf0a57ad038c0172ebca7ee1f2bfcd06761a016c
jethror1 <rainford1995@gmail.com>
2021-01-15 14:04:39 +0000

    docs

---
+++ ga_core/example_config.py
@@ -0,0 +1,17 @@
+SECRET_KEY = ""
+
+AUTH_TOKEN = ""
+
+EMAIL_USER = ""
+
+EMAIL_PASSWORD = ""
+
+GOOGLE_ANALYTICS = ""
+
+PROD_HOST = ""
+
+DEBUG_HOST = [""]
+
+PROD_DATABASE = ""
+
+DEBUG_DATABASE = ""
