7317bdbde26b02af343221adddace7a00895a102
jethror1 <rainford1995@gmail.com>
2021-03-04 10:40:18 +0000

    update example config

--- ga_core/example_config.py
+++ ga_core/example_config.py
@@ -1,17 +1,21 @@
+# django secret key
 SECRET_KEY = ""
 
+# DNAnexus auth token
 AUTH_TOKEN = ""
 
+# email account and sendgrid key for account emails
 EMAIL_USER = ""
-
 EMAIL_PASSWORD = ""
+SEND_GRID_API_KEY = ""
 
 GOOGLE_ANALYTICS = ""
 
+# django database settings
 PROD_HOST = ""
-
 DEBUG_HOST = [""]
-
 PROD_DATABASE = ""
-
 DEBUG_DATABASE = ""
+
+# path to primer3 python script, used for primer designer
+primer3_path = ""
