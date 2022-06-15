04e85d4330a5e7d71fcf7a6432b7793dbdccc672
Jason <jason_ling95@hotmail.com>
2022-06-15 12:01:16 +0100

    delete django-q part

--- DNAnexus_to_igv/tasks.py
+++
@@ -1,9 +0,0 @@
-# task to update bam / cnv json file
-from .find_bam import get_002_projects, find_dx_bams
-
-def update_json():
-    proj_list, proj_name = get_002_projects()
-    find_dx_bams(proj_list, proj_name)
-
-update_json()
-        
