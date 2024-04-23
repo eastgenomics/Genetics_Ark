c6d97d6fbb3ccf8ee7ac3d0e7501eaacc86ee5ad
JasonLing95 <jason_ling95@hotmail.com>
2024-04-23 12:32:16 +0100

    IN-569 include primer name in output pdf

--- primer_designer/services.py
+++ primer_designer/services.py
@@ -18,6 +18,8 @@ def call_subprocess(output_directory: str, regions: str, output_name) -> bool:
 
         primer_cmd = f"python3 {PRIMER_PATH}/primer_designer_region.py {cmd}"
 
+        print(primer_cmd)
+
         # call subprocess to run primer3
         call = subprocess.run(
             primer_cmd.split(),
@@ -38,34 +40,34 @@ def call_subprocess(output_directory: str, regions: str, output_name) -> bool:
     return True, {}
 
 
-def _format_region(region, dir):
+def _format_region(region: str, dir: str) -> str:
     """
     Format region from input form as command (str) for primer designer
 
     Args: region (str): region passed from Input Form
     Returns: cmd (str): formatted str of cmd for primer designer
     """
+    region = region.replace('\t', ' ')  # deal with tab-delimited inputs
+
     if region.count(":") > 1:
         # format for fusion design, will be in format
         # chr:pos:side:strand chr:pos:side:strand build 'fusion'
-        b1, b2, build = region.split()
-        cmd = f'--fusion --b1 {b1} --b2 {b2} --{build} -d {dir} -o {b1.replace(":", "_")}_{b2.replace(":", "_")}_{build}'
+        primer_namer, b1, b2, build = region.split()
+        cmd = f'--fusion --b1 {b1} --b2 {b2} --{build} -d {dir} -o {primer_namer}_{b1.replace(":", "_")}_{b2.replace(":", "_")}_{build}'
     else:
         # either position or range design
         if "-" in region:
             # range design, will be in format chr:pos1-pos2 build
-            region, build = region.split()
+            primer_name, region, build = region.split()
             chr, pos = region.split(":")
             pos1, pos2 = pos.split("-")
 
-            cmd = f"-c {chr} -r {pos1} {pos2} --{build} -d {dir} -o {chr}_{pos1}_{pos2}_{build}"
+            cmd = f"-c {chr} -r {pos1} {pos2} --{build} -d {dir} -o {primer_name}_{chr}_{pos1}_{pos2}_{build}"
         else:
             # normal position design, in format chr:pos build
-            region, build = region.split()
+            primer_name, region, build = region.split()
             chr, pos = region.split(":")
 
-            cmd = (
-                f"-c {chr} -p {pos} --{build} -d {dir} -o {chr}_{pos}_{build}"
-            )
+            cmd = f"-c {chr} -p {pos} --{build} -d {dir} -o {primer_name}_{chr}_{pos}_{build}"
 
     return cmd
